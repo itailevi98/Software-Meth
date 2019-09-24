cde56c1de9251365e7f1d8a2e1e7b6e01f3c1343
youngchoi211 <joshchoi9429@gmail.com>
2019-09-24 16:33:17 -0400

    Basic list

--- Controller.java
+++ Controller.java
@@ -1,16 +1,23 @@
 package SongLib;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
 
 public class Controller {
 
     @FXML
-    private Label label;
-
+    ListView<String> listView;
+    
+    private ObservableList<String> obsList;
+    
+  
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Bye, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + "." + "IceJJFish Rocks");
+
+    obsList=FXCollections.observableArrayList("Dead Presidents", "Thought you wuz nice", "Blackout", "Dancing in the Rain","Thieves in the Night");
+    listView.setItems(obsList);
+    
+    
     }
-}
+}	
--- Main.java
+++ Main.java
@@ -5,14 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+//    	Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+//    	FXMLLoader loader= new FXMLLoader();
+    
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("hellofx.fxml"));
+    	Parent root = (Parent) loader.load();
+    	    	
+        Controller controller = loader.getController();
+        controller.initialize();
+        
+        Scene scene = new Scene(root,400, 300);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
