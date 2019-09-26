5f37bcb7cb7f84cbba95668d41e4bb4b1414162c
Itai Levi <itailevi@nbp-10-196.nbp.ruw.rutgers.edu>
2019-09-26 17:11:54 -0400

    delete functionality

--- Controller.java
+++ Controller.java
@@ -8,7 +8,7 @@ import javafx.event.*;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 //import javafx.beans.value.ChangeListener;
-//import java.util.Optional;
+import java.util.Optional;
 
 
 public class Controller {
@@ -28,7 +28,7 @@ public class Controller {
     public TextField newAlbumName = new TextField();
     public TextField newYearDate = new TextField();
     public Button saveSong = new Button();
-    public Button deleteSong = new Button();
+    public Button deleteButton = new Button();
     public Button cancelButton = new Button();
    
   
@@ -53,6 +53,8 @@ public class Controller {
     	
     	
     	editButton.setOnAction(this::editSongDetails);
+    	deleteButton.setOnAction(this::deleteSong);
+    	
     	
     	
     	
@@ -71,19 +73,7 @@ public class Controller {
 		
 		saveSong.setVisible(false);
     	
-    	//listView.getSelectionModel().getSelectedItem();
-    	
-    	
-    	
-    	/*Alert alert = new Alert(AlertType.INFORMATION);
-    	alert.initOwner(primaryStage);
-    	alert.setTitle("Song Details");
-    	alert.setHeaderText("Selected song details: ");
-    	
-    	String content = "Name: " + listView.getSelectionModel().getSelectedItem();
-    	
-    	alert.setContentText(content);
-    	alert.showAndWait();*/
+
     }
     
     public void editSongDetails(ActionEvent event){
@@ -117,6 +107,34 @@ public class Controller {
     				editSongDetails(event);
     				return;
     			}
+    			
+    			Alert confirmation = new Alert(AlertType.CONFIRMATION);
+    			confirmation.initOwner(Main.primaryStage);
+    			confirmation.setTitle("Confirm?");
+    			confirmation.setHeaderText("Are you sure you want to edit the song's details?");
+    			
+    			ButtonType confirmEditSong = new ButtonType("Save Song Edits");
+    			ButtonType confirmCancelEdit = new ButtonType("Cancel Changes");
+    			confirmation.getButtonTypes().setAll(confirmEditSong, confirmCancelEdit);
+    			
+    			Optional<ButtonType> result = confirmation.showAndWait();
+    			
+    			if(result.get() == confirmCancelEdit) {
+    				newSongName.setVisible(false);
+        			newArtistName.setVisible(false);
+        			newAlbumName.setVisible(false);
+        			newYearDate.setVisible(false);
+        			
+        			saveSong.setVisible(false);
+        			cancelButton.setVisible(false);
+        			return;
+    				
+    			}
+
+    			
+    			
+    			
+    			
     			songName.setText(newSongName.getText());
     			artistName.setText(newArtistName.getText());
     			albumName.setText(newAlbumName.getText());
@@ -162,8 +180,39 @@ public class Controller {
     
     
     public void deleteSong(ActionEvent event) {
+    	Alert confirmation = new Alert(AlertType.CONFIRMATION);
+    	confirmation.initOwner(Main.primaryStage);
+    	confirmation.setTitle("Delete Song?");
+    	confirmation.setHeaderText("Are you sure you want to delete this song?");
     	
+    	ButtonType yesDelete = new ButtonType("Yes");
+    	ButtonType noDelete = new ButtonType("No");
     	
+    	confirmation.getButtonTypes().setAll(yesDelete, noDelete);
+    	
+    	Optional<ButtonType> result = confirmation.showAndWait();
+    	if(result.get() == noDelete) return;
+    	else {
+    		
+        	int index = listView.getSelectionModel().getSelectedIndex();
+        	if(index == 0) {
+        		listView.getItems().remove(index);
+        		listView.getSelectionModel().select(0);
+        		showItem(Main.primaryStage);
+        	}
+        	
+        	
+        	else if(index == listView.getItems().size() - 1) {
+        		listView.getSelectionModel().selectPrevious();
+        		listView.getItems().remove(index);
+        		showItem(Main.primaryStage);
+        		
+        	}
+        	
+        	
+    		
+    		
+    	}
     	
     }
     
